7366edacc8bb91bc6a8f5787a869e76c552c4803
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@stud.fh-campuswien.ac.at>
2020-08-13 20:12:37 +0200

    name and identification implemented

--- components/Form/Form.vue
+++ components/Form/Form.vue
@@ -1,51 +1,61 @@
 <template>
   <form>
     <v-text-field
-      v-model="name"
-      :error-messages="nameErrors"
-      :counter="10"
-      label="Name"
+      v-model="firstName"
+      :class="{ 'is-invalid': submitted && $v.firstName.$error }"
+      :error-messages="firstNameErrors"
+      label="First Name"
       required
-      @input="$v.name.$touch()"
-      @blur="$v.name.$touch()"
+    ></v-text-field>
+    <v-text-field
+      v-model="lastName"
+      :class="{ 'is-invalid': submitted && $v.lastName.$error }"
+      :error-messages="lastNameErrors"
+      label="Last Name"
+      required
     ></v-text-field>
     <v-text-field
       v-model="email"
+      :class="{ 'is-invalid': submitted && $v.email.$error }"
       :error-messages="emailErrors"
       label="E-mail"
       required
-      @input="$v.email.$touch()"
-      @blur="$v.email.$touch()"
     ></v-text-field>
     <v-select
       v-model="select"
       :items="items"
       :error-messages="selectErrors"
-      label="Item"
+      label="Type of Identification"
       required
-      @change="$v.select.$touch()"
-      @blur="$v.select.$touch()"
     ></v-select>
+    <v-text-field
+      v-if="select != null"
+      v-model="id"
+      :class="{ 'is-invalid': submitted && $v.id.$error }"
+      :error-messages="idErrors"
+      label="Identification Number"
+      required
+    ></v-text-field>
     <v-checkbox
       v-model="checkbox"
       :error-messages="checkboxErrors"
       label="Do you agree?"
       required
-      @change="$v.checkbox.$touch()"
-      @blur="$v.checkbox.$touch()"
     ></v-checkbox>
 
-    <v-btn class="mr-4" @click="submit">submit</v-btn>
+    <v-btn class="mr-4" @click.prevent="submit">submit</v-btn>
     <v-btn @click="clear">clear</v-btn>
   </form>
 </template>
 
 <script>
-import { required, maxLength, email } from 'vuelidate/lib/validators'
+import { required, email } from 'vuelidate/lib/validators'
 
 export default {
   validations: {
-    name: { required, maxLength: maxLength(10) },
+    firstName: { required },
+    lastName: { required },
+    id: { required },
     email: { required, email },
     select: { required },
     checkbox: {
@@ -56,11 +66,14 @@ export default {
   },
   data() {
     return {
-      name: '',
+      firstName: '',
+      lastName: '',
+      id: '',
       email: '',
       select: null,
-      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+      items: ['Driving Licence', 'Passport', 'Personal ID', 'other'],
       checkbox: false,
+      submitted: false,
     }
   },
 
@@ -77,26 +90,48 @@ export default {
       !this.$v.select.required && errors.push('Item is required')
       return errors
     },
-    nameErrors() {
+    firstNameErrors() {
       const errors = []
-      if (!this.$v.name.$dirty) return errors
-      !this.$v.name.maxLength &&
-        errors.push('Name must be at most 10 characters long')
-      !this.$v.name.required && errors.push('Name is required.')
+      if (!this.$v.firstName.$dirty) return errors
+      !this.$v.firstName.minLength &&
+        errors.push('Name must be at least 2 characters long')
+      !this.$v.firstName.required && errors.push('First name is required.')
+      return errors
+    },
+    lastNameErrors() {
+      const errors = []
+      if (!this.$v.lastName.$dirty) return errors
+      !this.$v.lastName.minLength &&
+        errors.push('Name must be at least 2 characters long')
+      !this.$v.lastName.required && errors.push('Last name is required.')
       return errors
     },
     emailErrors() {
       const errors = []
       if (!this.$v.email.$dirty) return errors
-      !this.$v.email.email && errors.push('Must be valid e-mail')
+      !this.$v.email && errors.push('Must be valid e-mail')
       !this.$v.email.required && errors.push('E-mail is required')
+      return errors
+    },
+    idErrors() {
+      const errors = []
+      if (!this.$v.id.$dirty) return errors
+      !this.$v.id.required && errors.push('ID is required.')
       return errors
     },
   },
 
   methods: {
     submit() {
+      this.submitted = true
+      console.log(this.select)
       this.$v.$touch()
+      if (this.$v.$invalid) {
+        this.submitStatus = 'ERROR'
+      } else {
+        this.submitStatus = 'OK'
+        console.log('submit')
+      }
     },
     clear() {
       this.$v.$reset()
